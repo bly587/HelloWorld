a5e8797be961c1fd9cc6bcebde0777242c4758fc
tmitchell <tmitchell@chapman.edu>
2019-09-03 21:55:06 +0000

    finished assignment

---
+++ helloWorld.cpp
@@ -0,0 +1,22 @@
+/*
+Titan Mitchell
+2327159
+tmitchell@chapman.edu
+CPSC 350 - 02
+Assignment "HelloWorld"
+
+Executes a program that prints out "Hello World" to the user
+*/
+#include <iostream>
+using namespace std;
+
+/*
+main method
+*/
+int main(int argc, char** argr)
+{
+  //print out Hello World to user and create a new line
+  cout << "Hello World" << endl;
+  //return 0 to show good run
+  return 0;
+}
